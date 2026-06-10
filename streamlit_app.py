7f74d7e050ce968ea95c08fc378ca18a7592704d
Edguns88 <edguns88@gmail.com>
2026-06-10 09:28:02 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ st.set_page_config(
 col1, col2 = st.columns([1, 3])
 
 with col1:
-    st.image("logo_ciencia_datos.png", width=200)
+    st.image("logo_ciencia_datos.png", width=300)
 
 with col2:
     st.title("Automatizador de Microcurrículos")
